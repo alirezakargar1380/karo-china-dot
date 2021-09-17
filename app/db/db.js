8bba6b81f4bf36ce6311752c8ba538715dd989ba
alireza kargar <imanesche@gmail.com>
2021-09-17 12:43:41 +0430

    create db connection with mongoose

---
+++ app/db/db.js
@@ -0,0 +1,13 @@
+// mongodb://localhost:27017/test
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/test", {
+  useNewUrlParser: true
+})
+    .then((res) => {
+      console.log(res.connections)
+      console.log("connected...")
+    })
+    .catch((e) => { console.log(e) })
+
+module.exports = mongoose
